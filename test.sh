e5fa8808935c373947bfdf798b81bd38de2dfbac
Alan Schexnayder <alan.schexnayder@gmail.com>
2018-03-20 12:50:47 -0400

    Delete classification hexdump

--- test.sh
+++ test.sh
@@ -1,53 +1,56 @@
 #!/bin/bash
 # Script to compile and test cs361 projects
-#Alan Schexnayder, 03/16/18, V2.0
+# Alan Schexnayder, 03/20/18, V2.0
 
-## CHANGE mapper TO YOUR EXECUTABLE NAME ##
+## change "mapper" to whatever your executable name is, i.e.  ##
+## for PA5 it would be, mapper-with-queues                    ##
 EXECUTABLE=mapper
 
-#color variables
+# color variables
 RED='\033[1;31m'
 LIGHTYELLOW='\033[0;33m'
 GREEN='\033[1;32m'
 WHITE='\033[1;37m'
 YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
+NC='\033[0m'           # No color
 
 echo "Building..."
-make             #compiles
+make                   #compiles
 
 test () {
 	echo -e "${WHITE}Beginning testing with $1 input...${NC}"
 	echo "Deleting your old queues..."
-	touch /dev/mqueue/blank                #gives the next line something to delete
-	rm /dev/mqueue/*                       #clears any queues
-	rm -rf map classification actual/$1/*  #deletes any previously generated files
-	./$EXECUTABLE inputs/$1           #runs project using $1 input
+	touch /dev/mqueue/blank                # gives the next line something to delete
+	rm /dev/mqueue/*                       # clears any queues
+	rm -rf map classification actual/$1/*  # deletes any previously generated files
+	./$EXECUTABLE inputs/$1                # runs project using $1 input
 
-	generate_diffs $1 classification      #generates the diffs for the classification file
-	generate_diffs $1 map                 #generates the diffs for the map file
+	generate_diffs $1 classification       # generates the diffs for the classification file
+	generate_diffs $1 map                  # generates the diffs for the map file
 }
 
 generate_diffs() {
-	#creates a hexdump of the generated $2 file
+	# $1 refers to the input file
+	# $2 refers to the map/classifcation file
+	# creates a hexdump of the generated map or classifcation file
 	hexdump -Cv $2 > actual/$1/$2_hexdump.txt
 
-	#compares the difference between the hexdumps and sends them to a diff file
+	# compares the difference between the hexdumps and sends them to a diff file
 	diff actual/$1/$2_hexdump.txt expected/$1/expected_$2_hexdump.txt > actual/$1/$2_hexdump.diff
-	if [ -s actual/$1/$2_hexdump.diff ]; #if file isn't empty
+	if [ -s actual/$1/$2_hexdump.diff ]; # if file isn't empty
 	then
-		EXPECTEDSIZE=$(wc -c <"expected/$1/expected_$2_hexdump.txt") #calculate size of the expected file
-	  DIFFSIZE=$(wc -c <"actual/$1/$2_hexdump.diff")               #calculate size of the diff file
+		EXPECTEDSIZE=$(wc -c <"expected/$1/expected_$2_hexdump.txt") # calculate size of the expected file
+	  DIFFSIZE=$(wc -c <"actual/$1/$2_hexdump.diff")               # calculate size of the diff file
 		echo -e "${RED}$2 file is wrong:${NC}"
-		DIFFLINES=$(head -5 actual/$1/$2_hexdump.diff)                           #prints out the first few lines of the diff
+		DIFFLINES=$(head -5 actual/$1/$2_hexdump.diff)               # prints out the first few lines of the diff
 		 echo -e "${LIGHTYELLOW}$DIFFLINES${NC}"
 
 		WRONGLINES=$(grep \< -o actual/$1/$2_hexdump.diff | wc -l)
 		TOTALLINES=$(wc -l < expected/$1/expected_$2_hexdump.txt)
 		RIGHTLINES=`expr $TOTALLINES - $WRONGLINES`
 
-		PERCENT=$(echo $RIGHTLINES / $TOTALLINES \* 100| bc -l)   #calculates a percentage
-		PERCENT=$(echo $PERCENT | awk '{printf("%.1f\n",$1)}')    #Rounds to 1 decimal difference
+		PERCENT=$(echo $RIGHTLINES / $TOTALLINES \* 100| bc -l)      # calculates a percentage
+		PERCENT=$(echo $PERCENT | awk '{printf("%.1f\n",$1)}')       # Rounds to 1 decimal difference
 
 	echo -e "Estimated percent correct is: ${YELLOW}$PERCENT%${NC}"
 
@@ -55,6 +58,6 @@ generate_diffs() {
 		echo -e "${GREEN}$2 works with $1!${NC}"
 	fi
 }
-test sample_clusters
-test alternating
-test multi_jpg
+test sample_clusters  # runs with "sample_clusters" inpput
+test alternating      # runs with "alternating" inpput
+test multi_jpg        # runs with "multi_jpg" inpput
